25a0c560a59424e9b4acd1c351a30cd4b31b2b6c
Alex Yang <hw.yang@berkeley.edu>
2020-02-12 19:09:10 -0800

    0212

--- start.py
+++ start.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import h5py
 import torch
 import matplotlib
@@ -16,17 +10,9 @@ from math import pi, cos, sin
 from scipy.integrate import quad
 from sklearn.manifold import TSNE
 
-
-# In[4]:
-
-
 f = h5py.File('./MNIST data/train_0.h5', 'r')
 data = f['data']
 label = f['label']
-
-
-# In[6]:
-
 
 # parameters
 numOfClass = 10
@@ -35,188 +21,6 @@ numOfGaussian = 50
 etaW = 1e-3
 etaC = 1e-2
 epsilon = 1
-
-
-# In[7]:
-
-
-# data cleaning: choose images from each of the classes
-selectedData, selectedLabel = ImageChoosing(numOfImagesEachClass, numOfClass)
-selectedData = selectedData[:, :, 0:2]
-#selectedData = selectedData[:, :, 0:3]
-
-# putting 1000 images together
-finalData = np.zeros((256 * numOfImagesEachClass * numOfClass, 2))
-finalData = selectedData.reshape((256 * numOfImagesEachClass * numOfClass, 2))
-
-
-# In[24]:
-
-
-# initializing C
-gmm = GaussianMixture(n_components = numOfGaussian)
-gmm.fit(finalData)
-means = gmm.means_
-covariances = gmm.covariances_
-C = covariances
-C = torch.from_numpy(C)
-C.requires_grad_(True)
-C = NormalizingC(C)
-print(C)
-
-
-# In[29]:
-
-
-# initializing W
-W = np.random.random_sample((numOfClass * numOfImagesEachClass, numOfGaussian))
-
-# normalizing W
-W = torch.from_numpy(W)
-temp = NormalizingW(W)
-W = temp
-W.requires_grad_(True)
-W.retain_grad()
-print(W)
-
-
-# In[10]:
-
-
-# graph
-graphW = np.random.random_sample((numOfClass * numOfImagesEachClass, numOfGaussian))
-for i in range(graphW.shape[0]):
-    for j in range(graphW.shape[1]):
-        graphW[i][j] = W[i][j]
-tsne = TSNE(n_components=2, random_state=0)
-W_2d = tsne.fit_transform(graphW)
-target_ids = range(numOfClass * numOfImagesEachClass)
-
-from matplotlib import pyplot as plt
-plt.figure(figsize=(6, 5))
-label = selectedLabel.astype(int)
-colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'yellow', 'orange', 'purple']
-for i in range(numOfClass * numOfImagesEachClass):
-    plt.scatter(W_2d[i,0], W_2d[i,1], color = colors[label[i]])
-plt.legend()
-plt.show()
-
-
-# In[30]:
-
-
-# initializing flag
-flag = 1
-
-# compute X
-X = GenerateX()
-X = torch.from_numpy(X)
-
-# compute Xhat
-Xhat = GenerateXhat()
-
-
-# In[31]:
-
-
-# compute objective function E
-E = GenerateE()
-E.requires_grad_(True)
-
-
-# In[1]:
-
-
-# begin gradient descent
-lambda_ = 0
-YCNew = C
-YW = torch.sqrt(W)
-YWNew = W
-
-while True:
-    print("Error of iteration", flag, "is:", E.item())
-    EOld = E
-    lambdaNew_ = (1 + np.sqrt(1 + 4 * np.power(lambda_, 2))) / 2
-    gama = (1 - lambda_) / lambdaNew_
-    lambda_ = lambdaNew_
-    WGrad = torch.autograd.grad(E, W, create_graph = True, allow_unused = True)[0]
-    CGrad = torch.autograd.grad(E, C, create_graph = True, allow_unused = True)[0]
-    CGrad = NormalizingCGrad(CGrad)
-    WGrad = NormalizingWGrad(WGrad)
-    YWNew = YW - torch.mul(WGrad, etaW)
-    YWNew = (1 - gama) * YWNew + gama * YW
-    YW = YWNew
-    W = torch.pow(YWNew, 2)
-    W = NormalizingW(W)
-    for j in range(numOfGaussian):
-        YCNew[j] = ExponentialMap(C[j], - etaC * CGrad[j])
-        C[j] = ExponentialMap(YCNew[j], gama * LogMap(YCNew[j], C[j]))
-    Xhat = GenerateXhat()
-    E = GenerateE()
-    if flag == 50:
-        print("The Final Error is:", E.item())
-        break
-    flag += 1
-
-
-# In[54]:
-
-
-# graph tsne
-graphW = np.random.random_sample((numOfClass * numOfImagesEachClass, numOfGaussian))
-for i in range(graphW.shape[0]):
-    for j in range(graphW.shape[1]):
-        graphW[i][j] = W[i][j]
-tsne = TSNE(n_components=2, random_state=0)
-W_2d = tsne.fit_transform(graphW)
-target_ids = range(numOfClass * numOfImagesEachClass)
-
-plt.figure(figsize=(6, 5))
-label = selectedLabel.astype(int)
-colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'yellow', 'orange', 'purple']
-for i in range(numOfClass * numOfImagesEachClass):
-    plt.scatter(W_2d[i,0], W_2d[i,1], color = colors[label[i]])
-plt.legend()
-plt.savefig('tsne.png')
-plt.show()
-
-
-# In[51]:
-
-
-from sklearn.decomposition import PCA
-
-data = graphW
-pca = PCA(n_components=3, svd_solver='full')
-result = pca.fit_transform(data)
-plt.figure(figsize=(6, 5))
-label = selectedLabel.astype(int)
-colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'yellow', 'orange', 'purple']
-for i in range(numOfClass * numOfImagesEachClass):
-    plt.scatter(result[i,0], result[i,1], color = colors[label[i]])
-plt.legend()
-plt.show()
-
-
-# In[52]:
-
-
-print(pca.explained_variance_ratio_)
-
-
-# In[24]:
-
-
-count = 0
-for i in range(1000):
-    for j in range(50):
-        if W[i][j] < 0.001:
-            count += 1
-print(count/(1000 * 50))
-
-
-# In[23]:
-
 
 # helper functions
 def ImageChoosing(numEach, numOfClass):
@@ -278,14 +82,12 @@ def sqrtMatrix(matrix):
     u, s, v = torch.svd(matrix)
     newS = torch.sqrt(torch.diag(s))
     return torch.mm(torch.mm(u, newS), v.t())
-    
+
 def GenerateE():
     E = 0
     for i in range(numOfClass * numOfImagesEachClass):
-        #temp = torch.trace(torch.mm(torch.inverse(X[i]), Xhat[i]) + torch.mm(torch.inverse(Xhat[i]), X[i]))
-        #E += (1 / 4) * (temp - 4)
-        temp = torch.norm(X[i] - Xhat[i])
-        E += temp
+        temp = torch.trace(torch.mm(torch.inverse(X[i]), Xhat[i]) + torch.mm(torch.inverse(Xhat[i]), X[i]))
+        E += (1 / 4) * (temp - 4)
     return E
 
 def ExponentialMap(x, V):
@@ -300,7 +102,7 @@ def LogMap(x, Y):
     newS = torch.diag(torch.log(s))
     midTerm = torch.mm(torch.mm(u, newS), u.t())
     return torch.mm(torch.mm(sqrtMatrix(x), midTerm), sqrtMatrix(x))
-    
+
 def NormalizingWGrad(grad):
     temp = grad.clone()
     for i in range(grad.shape[0]):
@@ -345,3 +147,116 @@ def CheckYCNew(C):
             if C[i][j][j] < 0:
                 print("YCNew has negative components on the diag on the", j, "row of ", i, "element")
 
+# data cleaning: choose images from each of the classes
+selectedData, selectedLabel = ImageChoosing(numOfImagesEachClass, numOfClass)
+selectedData = selectedData[:, :, 0:2]
+#selectedData = selectedData[:, :, 0:3]
+
+# putting 1000 images together
+finalData = np.zeros((256 * numOfImagesEachClass * numOfClass, 2))
+finalData = selectedData.reshape((256 * numOfImagesEachClass * numOfClass, 2))
+
+# initializing C
+gmm = GaussianMixture(n_components = numOfGaussian)
+gmm.fit(finalData)
+means = gmm.means_
+covariances = gmm.covariances_
+C = covariances
+C = torch.from_numpy(C)
+C.requires_grad_(True)
+C = NormalizingC(C)
+print(C)
+
+# initializing W
+W = np.random.random_sample((numOfClass * numOfImagesEachClass, numOfGaussian))
+
+# normalizing W
+W = torch.from_numpy(W)
+temp = NormalizingW(W)
+W = temp
+W.requires_grad_(True)
+W.retain_grad()
+print(W)
+
+# graph
+graphW = np.random.random_sample((numOfClass * numOfImagesEachClass, numOfGaussian))
+for i in range(graphW.shape[0]):
+    for j in range(graphW.shape[1]):
+        graphW[i][j] = W[i][j]
+tsne = TSNE(n_components=2, random_state=0)
+W_2d = tsne.fit_transform(graphW)
+target_ids = range(numOfClass * numOfImagesEachClass)
+
+from matplotlib import pyplot as plt
+plt.figure(figsize=(6, 5))
+label = selectedLabel.astype(int)
+colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'yellow', 'orange', 'purple']
+for i in range(numOfClass * numOfImagesEachClass):
+    plt.scatter(W_2d[i,0], W_2d[i,1], color = colors[label[i]])
+plt.legend()
+plt.savefig('before,png')
+plt.show()
+
+# initializing flag
+flag = 1
+
+# compute X
+X = GenerateX()
+X = torch.from_numpy(X)
+
+# compute Xhat
+Xhat = GenerateXhat()
+
+# compute objective function E
+E = GenerateE()
+E.requires_grad_(True)
+
+# begin gradient descent
+lambda_ = 0
+YCNew = C
+YW = torch.sqrt(W)
+YWNew = W
+
+while True:
+    print("Error of iteration", flag, "is:", E.item())
+    EOld = E
+    lambdaNew_ = (1 + np.sqrt(1 + 4 * np.power(lambda_, 2))) / 2
+    gama = (1 - lambda_) / lambdaNew_
+    lambda_ = lambdaNew_
+    WGrad = torch.autograd.grad(E, W, create_graph = True, allow_unused = True)[0]
+    CGrad = torch.autograd.grad(E, C, create_graph = True, allow_unused = True)[0]
+    CGrad = NormalizingCGrad(CGrad)
+    WGrad = NormalizingWGrad(WGrad)
+    YWNew = YW - torch.mul(WGrad, etaW)
+    YWNew = (1 - gama) * YWNew + gama * YW
+    YW = YWNew
+    W = torch.pow(YWNew, 2)
+    W = NormalizingW(W)
+    for j in range(numOfGaussian):
+        YCNew[j] = ExponentialMap(C[j], - etaC * CGrad[j])
+        C[j] = ExponentialMap(YCNew[j], gama * LogMap(YCNew[j], C[j]))
+    Xhat = GenerateXhat()
+    E = GenerateE()
+    if flag == 50:
+        print("The Final Error is:", E.item())
+        break
+    flag += 1
+
+
+# graph tsne
+graphW = np.random.random_sample((numOfClass * numOfImagesEachClass, numOfGaussian))
+for i in range(graphW.shape[0]):
+    for j in range(graphW.shape[1]):
+        graphW[i][j] = W[i][j]
+tsne = TSNE(n_components=2, random_state=0)
+W_2d = tsne.fit_transform(graphW)
+target_ids = range(numOfClass * numOfImagesEachClass)
+
+plt.figure(figsize=(6, 5))
+label = selectedLabel.astype(int)
+colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'yellow', 'orange', 'purple']
+for i in range(numOfClass * numOfImagesEachClass):
+    plt.scatter(W_2d[i,0], W_2d[i,1], color = colors[label[i]])
+plt.legend()
+plt.savefig('tsne.png')
+plt.show()
